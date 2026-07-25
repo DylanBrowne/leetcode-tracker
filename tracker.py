2abe0795cdbf80b4c77a0825d9643d6a89e90b3b
dylan <dylanb.connect@gmail.com>
2026-07-25 11:57:23 -0700

    Added SQLite table that includes id, title, difficulty, pattern, and confidence.

--- tracker.py
+++ tracker.py
@@ -1,1 +1,35 @@
- 
+ # I need to import sqlite3, establish a connection to the database.db, and print a message to prove it was successful.
+# SQLite is good for keeping track of data within a database
+
+import sqlite3
+
+# define connection and cursor. 
+
+# Connections are used to connect to a database (database.db)
+try:
+    connection = sqlite3.connect("database.db")
+    print("Established baseline SQLite connection pipeline")
+
+    # Create a cursor to execute SQL statements and fetch results from SQL queries
+    cursor = connection.cursor()
+
+
+    # Create a database table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS problems (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            difficulty TEXT NOT NULL,
+            pattern TEXT NOT NULL,
+            confidence INTEGER NOT NULL
+        )
+    """)
+
+    connection.commit()
+    print("Database table successfully verified/created.")
+except Exception as e:
+    print(f"Something went wrong: {e}")
+
+finally:
+    if 'connection' in locals():
+        connection.close()
